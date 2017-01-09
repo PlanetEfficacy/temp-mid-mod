e62f61bc433abd9b055e21d1341c9e573ba0d7bd
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 12:20:26 -0700

    Add logout feature

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe "user signs out" do
   scenario "and is redirected back to login screen" do
     user = create :user
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    visit links_path
+    visit login_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Sign In"
 
     expect(page).to have_content("Sign Out")
     expect(page).to_not have_content("Sign In")
 
     click_link "Sign Out"
-
     expect(current_path).to eq(login_path)
+    expect(page).to have_content("Sign In")
+    expect(page).to have_content("Sign Up")
   end
 end
