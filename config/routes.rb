1debd15a8b0b08d8edea7c794716a972203ccb2d
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 10:19:03 -0700

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
 
+  post '/login', to: 'sessions#create'
+
   resources :links, only: [:index]
+  resources :users, only: [:new, :create]
 
   namespace :api do
     namespace :v1 do
