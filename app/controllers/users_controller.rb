678d8fad0aecf0ddec6de14238104110b8887d1c
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 10:25:13 -0700

    Add user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:uid] = user.id
+      redirect_to links_path
+    else
+
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
