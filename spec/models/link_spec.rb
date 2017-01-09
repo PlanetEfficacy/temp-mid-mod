fea758fa21c1a501f73b355f761f6e02232e2e7e
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 12:30:03 -0700

    Add link valid url validation and test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Link, type: :model do
   it { should validate_presence_of(:url) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:read) }
+  it "requires a valid url" do
+    link = Link.new(title: "My Link", url: "www.google.com", read: true)
+    expect(link.save).to eq(false)
+  end
 end
