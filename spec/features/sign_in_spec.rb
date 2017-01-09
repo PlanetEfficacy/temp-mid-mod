e38a8e66d15eb7de1b5e5589c349db6cbcf6edc2
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 12:01:08 -0700

    Add sad path for bad email on login

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "user signs in" do
     expect(current_path).to eq(links_path)
     expect(page).to have_content("Successfully logged in!")
   end
+
   context "enters incorrect password" do
     scenario "and receives a flash message" do
       user = create :user
@@ -23,4 +24,18 @@ RSpec.describe "user signs in" do
       expect(page).to have_content("Email and or password are not recognized.")
     end
   end
+
+  context "enters incorrect email" do
+    scenario "and receives a flash message" do
+      user = create :user
+      visit login_path
+      fill_in "Email", with: "something@example.com"
+      fill_in "Password", with: "password"
+      click_button "Sign In"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Email and or password are not recognized.")
+    end
+  end
+
 end
