5811d8ac715fe7cf6fbc17db5bb124bf4408917e
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 11:36:20 -0700

    Add user sign in feature test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "user signs in" do
-  xscenario "by entering email and password" do
+  scenario "and receives a flash message" do
+    user = create :user
+    visit login_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
 
+    expect(current_path).to eq(links_path)
+    expect(page).to have_content("Successfully logged in!")
   end
 end
