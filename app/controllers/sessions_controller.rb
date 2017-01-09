ee39694ac7a13702ea0f8c021ecbd020a32e4422
PlanetEfficacy <jspevack@gmail.com>
2017-01-09 12:00:11 -0700

    Add sad path for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
-    if @user && @user.authenticate(params[:password])
+    if @user
       session[:uid] = @user.id
       flash[:success] = "Successfully logged in!"
       redirect_to links_path
     else
+      flash[:error] = "Email and or password are not recognized."
+      render :new
     end
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe "user signs in" do
     expect(current_path).to eq(links_path)
     expect(page).to have_content("Successfully logged in!")
   end
+  context "enters incorrect password" do
+    scenario "and receives a flash message" do
+      user = create :user
+      visit login_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "password1"
+      click_button "Sign In"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Email and or password are not recognized.")
+    end
+  end
 end
